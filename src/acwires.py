fb03fda23ed9ee96fe70acd2e475f8e43b95f2ed
Richard Turner <rwturner@stanford.edu>
2013-12-17 13:31:00 -0800

    Added thick finite wires
    taken from Extravour thesis appendix

--- src/acwires.py
+++ src/acwires.py
@@ -5,7 +5,7 @@ Created on Tue Apr 23 22:54:24 2013
 @author: Will
 """
 
-from math import pi, sqrt
+from math import pi, sqrt, atan, atanh
 import numpy as np
 from acmconstants import MU_0
 
@@ -126,3 +126,52 @@ class NWire(Wire):
                 B_Gx=B_G*(suby[j] - y)
                 B_Gy=B_G*(x - subx[i])
         return np.array((B_Gx, B_Gy, 0))
+        
+class ThickFinWire(Wire):
+    '''Wire of finite length and width, no height'''
+    def __init__(self, name, length, width, height, current, x0,y0,z0):
+        Wire.__init__(self,name,length,width,height,current,1)
+        self.x0 = x0
+        self.y0 = y0
+        self.z0 = z0
+       
+    def bfieldcalc(self, x, y, z):
+        ''' Set up for a wire in the x, y plane parallel to x axis of length 2*L at y = R'''
+        #aliases
+        self.L = self.length / 2
+        self.R = self.y0
+        w = self.width
+        I = self.current
+        
+        G = (mu_0 * I / (4*pi*w))
+        B_y = -1*G * (__AuxBy__(x, y, z, w) + __AuxBy__(-1*x, y, z, w) - __AuxBy__(x, y, z, -1*w) - __AuxBy__(-1*x, y, z, -1*w))
+        B_z = G * (__AuxBz__(x, y, z, w) + __AuxBz__(-1*x, y, z, w) - __AuxBz__(x, y, z, -1*w) - __AuxBz__(-1*x, y, z, -1*w))
+        return np.array((0, B_y, B_z))
+    
+    def __AuxBy__(self, x, y, z, w):
+        x0 = self.L + x
+        y0 = self.R - y
+        num = x0*(y0 + 0.5*w)
+        den = z * sqrt(x0**2 + y0**2 + z**2)
+        return atan(num / den)
+        
+    def __AuxBz__(self, x, y, z, w):
+        x0 = self.L + x
+        y0 = self.R - y
+        num = sqrt(x0**2 + (y0 - 0.5*w)**2 + z**2)
+        return atanh(num / x0)
+        
+class ThickFinHWire(ThickFinWire):
+    def __init__(self, name, length, width, height, current, x0,y0,z0)
+        super().__init__(self, name, length, width, height, current, x0,y0,z0)
+    def bfieldcalc(self, x, y, z):
+        return super().bfieldcalc(self, x - self.x0, y, z - self.z0)
+
+class ThickFinVWire(ThickFinWire):
+    '''stored in the rotated frame; never access properties directly, write appropriate getters'''
+    def __init__(self, name, length, width, height, current, x0,y0,z0)
+        super().__init__(self, name, length, width, height, current, y0,-1*x0,z0)
+    def bfieldcalc(self, x, y, z):
+        '''Need to rotate the x, y axes for this...'''
+        rot_frame_field = super().bfieldcalc(self, x - self.x0, y, z - self.z0)
+        return np.array((-1*rot_frame_field[1], 0, rot_frame_field[2]))
