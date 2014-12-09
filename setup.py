d74558b6c1caec0c81e96dd77ff6b131a1eea9f6
Richard Turner <rwturner@stanford.edu>
2014-12-09 14:07:11 -0800

    Corrected package structure

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='bfieldsim',
     version='0.1dev',
     packages=['bfieldsim',],
-    license='GPL',
+    license='MIT',
     long_description=open('README.txt').read(),
     author='Richard W. Turner',
     author_email='rwturner@stanford.edu',
+    install_requires = ['numdifftools', 'numpy', 'matplotlib', 'scipy'],
     description='Code for simulating fields due to atom chips'
 )
